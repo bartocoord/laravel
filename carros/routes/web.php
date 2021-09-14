f9b63257cbfc479ffb411d9a39661dfc128173f7
bartocoord <barto.coord.area@gmail.com>
2021-09-14 12:20:29 -0300

    install laravel 6

---
+++ carros/routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+
+Route::get('/', function () {
+    // return view('welcome');
+    return "<h1>Olá</h1>";
+});
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+echo "
+<h1>tudo ok</h1>
+<h3>e configurado o git</h3>";
